864cdebc178675f108d63b76f226a88e20a99fa7
adadepythian <marceloadade@gmail.com>
2021-10-26 18:06:12 -0300

    Adding new file

---
+++ Backup_Restore/check_bkp_history.sql
@@ -0,0 +1,38 @@
+
+SELECT 
+CONVERT(CHAR(100), SERVERPROPERTY('Servername')) AS Server, 
+msdb.dbo.backupset.database_name, 
+msdb.dbo.backupset.backup_start_date, 
+msdb.dbo.backupset.backup_finish_date, 
+msdb.dbo.backupset.expiration_date, 
+CASE msdb..backupset.type 
+WHEN 'D' THEN 'Database' 
+WHEN 'L' THEN 'Log'
+WHEN 'I' THEN 'Differential'
+END AS backup_type, 
+msdb.dbo.backupset.backup_size, 
+msdb.dbo.backupmediafamily.logical_device_name, 
+msdb.dbo.backupmediafamily.physical_device_name, 
+msdb.dbo.backupset.name AS backupset_name, 
+msdb.dbo.backupset.description 
+FROM msdb.dbo.backupmediafamily 
+INNER JOIN msdb.dbo.backupset ON msdb.dbo.backupmediafamily.media_set_id = msdb.dbo.backupset.media_set_id 
+WHERE (CONVERT(datetime, msdb.dbo.backupset.backup_start_date, 102) >= GETDATE() - 7) 
+ORDER BY 
+msdb.dbo.backupset.database_name, 
+msdb.dbo.backupset.backup_finish_date 
+
+SELECT 
+sum(msdb.dbo.backupset.backup_size)/1024/1024, 
+sum(msdb.dbo.backupset.compressed_backup_size)/1024/1024
+FROM msdb.dbo.backupmediafamily 
+INNER JOIN msdb.dbo.backupset ON msdb.dbo.backupmediafamily.media_set_id = msdb.dbo.backupset.media_set_id 
+WHERE (CONVERT(datetime, msdb.dbo.backupset.backup_start_date, 102) >= GETDATE() - 1) 
+
+
+SELECT distinct
+left(msdb.dbo.backupmediafamily.physical_device_name,1) as BKPDrive
+FROM msdb.dbo.backupmediafamily 
+INNER JOIN msdb.dbo.backupset ON msdb.dbo.backupmediafamily.media_set_id = msdb.dbo.backupset.media_set_id 
+WHERE (CONVERT(datetime, msdb.dbo.backupset.backup_start_date, 102) >= GETDATE() - 7) 
+and msdb.dbo.backupmediafamily.physical_device_name not like '{%'
