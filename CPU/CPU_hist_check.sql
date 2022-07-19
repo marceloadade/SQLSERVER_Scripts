e60dda8a8bf811e259c8ca4c64cad1299c3e1384
Marcelo Adade <marceloadade@gmail.com>
2022-07-19 09:38:32 -0300

    Update CPU_hist_check.sql

--- CPU/CPU_hist_check.sql
+++ CPU/CPU_hist_check.sql
@@ -59,3 +59,53 @@ WHERE ring_buffer_type = 'RING_BUFFER_SCHEDULER_MONITOR'
 AND record LIKE '%SystemHealth%') AS t
 ) AS t
 ORDER BY record_id desc
+
+
+--if you need historic data, setup a run that run every hour with this code:
+
+--create objects if they don't exist:
+/*
+use msdb
+go
+
+create table CpuHistory(
+EventTime datetime2 not null,
+system_cpu_utilization tinyint not null,
+sql_cpu_utilization tinyint not null
+)
+
+create index CpuHistory_Clst_IX on CpuHistory(EventTime);
+*/
+--populate example:
+--delete from CpuHistory
+--cleanup:
+delete from CpuHistory where EventTime < dateadd(day,-35,getdate())
+
+
+--insert data for the last hour:
+DECLARE @ts_now bigint
+ 
+SELECT @ts_now = cpu_ticks / (cpu_ticks/ms_ticks)  FROM sys.dm_os_sys_info
+
+insert into msdb.dbo.CpuHistory(EventTime,system_cpu_utilization,sql_cpu_utilization)
+SELECT  EventTime,
+CASE WHEN system_cpu_utilization_post_sp2 IS NOT NULL THEN system_cpu_utilization_post_sp2 ELSE system_cpu_utilization_pre_sp2 END AS system_cpu_utilization,
+CASE WHEN sql_cpu_utilization_post_sp2 IS NOT NULL THEN sql_cpu_utilization_post_sp2 ELSE sql_cpu_utilization_pre_sp2 END AS sql_cpu_utilization
+FROM
+(
+SELECT top 60
+DATEADD (ms, -1 * (@ts_now - [timestamp]), GETDATE()) AS EventTime,
+100-record.value('(Record/SchedulerMonitorEvent/SystemHealth/SystemIdle)[1]', 'int') AS system_cpu_utilization_post_sp2,
+record.value('(Record/SchedulerMonitorEvent/SystemHealth/ProcessUtilization)[1]', 'int') AS sql_cpu_utilization_post_sp2 ,
+100-record.value('(Record/SchedluerMonitorEvent/SystemHealth/SystemIdle)[1]', 'int') AS system_cpu_utilization_pre_sp2,
+record.value('(Record/SchedluerMonitorEvent/SystemHealth/ProcessUtilization)[1]', 'int') AS sql_cpu_utilization_pre_sp2
+FROM (
+SELECT timestamp, CONVERT (xml, record) AS record
+FROM sys.dm_os_ring_buffers
+WHERE ring_buffer_type = 'RING_BUFFER_SCHEDULER_MONITOR'
+AND record LIKE '%SystemHealth%') AS t
+) AS t
+
+--check data:
+--select * from CpuHistory
+
